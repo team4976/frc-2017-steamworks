bc689220b6420019cd103053fd967c2e7168c293
Aavin <aavinsidhu@hotmaail.com>
2017-02-09 15:46:12 -0500

    Initial Commit

--- src/main/java/ca/_4976/steamworks/io/Inputs.java
+++ src/main/java/ca/_4976/steamworks/io/Inputs.java
@@ -5,7 +5,23 @@ import ca._4976.library.inputs.DigitalEncoder;
 
 public class Inputs {
 
+    public DigitalEncoder driveLeft;
+    public DigitalEncoder driveRight;
+    public DigitalEncoder shooter_encoder;
+
     public Inputs(AsynchronousRobot module) {
 
+        driveRight = new DigitalEncoder(module, 0, 1);
+        driveLeft = new DigitalEncoder(module, 2, 3);
+        shooter_encoder = new DigitalEncoder(module, 12, 13);
+
+        driveLeft.setReversed(true);
+        driveRight.setReversed(true);
+
+        driveLeft.setScalar(0.00031179492);
+        driveRight.setScalar(0.00031179492);
+
+        driveLeft.setMinRate(0);
+        driveRight.setMinRate(0);
     }
 }
--- src/main/java/ca/_4976/steamworks/io/Outputs.java
+++ src/main/java/ca/_4976/steamworks/io/Outputs.java
@@ -1,13 +1,31 @@
 package ca._4976.steamworks.io;
 
 import ca._4976.library.AsynchronousRobot;
+import ca._4976.library.outputs.CANMotor;
+import ca._4976.library.outputs.LinearActuator;
 import ca._4976.library.outputs.PWMMotor;
 import ca._4976.library.outputs.Solenoid;
-import edu.wpi.first.wpilibj.Compressor;
+import ca._4976.steamworks.subsystems.shooter_cock;
 
 public class Outputs {
 
+    public PWMMotor driveLeftFront;
+    public PWMMotor driveLeftRear;
+    public PWMMotor driveRightFront;
+    public PWMMotor driveRightRear;
+    public LinearActuator hood;
+    public CANMotor shooter;
+    public Solenoid solenoid;
+
     public Outputs(AsynchronousRobot module) {
 
+        driveLeftFront = new PWMMotor(module, 1, 0.02);
+        driveLeftRear = new PWMMotor(module, 2, 0.02);
+        driveRightFront = new PWMMotor(module, 0, 0.02);
+        driveRightRear = new PWMMotor(module, 3, 0.02);
+        shooter = new CANMotor(12);
+
+        solenoid = new Solenoid(module, 20, 1, 2);
+        hood = new LinearActuator(4);
     }
 }
