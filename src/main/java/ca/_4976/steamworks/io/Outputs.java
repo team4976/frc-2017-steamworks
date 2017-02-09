3569c29b0228ff546a6657fb5b8904eb9cb7b157
Grant Colwell <Grant Colwell>
2017-02-09 16:10:36 -0500

    Inital Commit

--- src/main/java/ca/_4976/steamworks/io/Outputs.java
+++ src/main/java/ca/_4976/steamworks/io/Outputs.java
@@ -3,11 +3,24 @@ package ca._4976.steamworks.io;
 import ca._4976.library.AsynchronousRobot;
 import ca._4976.library.outputs.PWMMotor;
 import ca._4976.library.outputs.Solenoid;
-import edu.wpi.first.wpilibj.Compressor;
 
 public class Outputs {
 
+    public PWMMotor driveLeftFront;
+    public PWMMotor driveLeftRear;
+    public PWMMotor driveRightFront;
+    public PWMMotor driveRightRear;
+
+    public Solenoid solenoid;
+
     public Outputs(AsynchronousRobot module) {
 
+        driveLeftFront = new PWMMotor(module, 1, 0.02);
+        driveLeftRear = new PWMMotor(module, 2, 0.02);
+        driveRightFront = new PWMMotor(module, 0, 0.02);
+        driveRightRear = new PWMMotor(module, 3, 0.02);
+
+
+        solenoid = new Solenoid(module, 20, 1, 2);
     }
 }
