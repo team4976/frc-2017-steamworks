86fd208c7cc02850f3ab15b9ddccc28eb0b0762f
Ivan Zdane <izdane@gmail.com>
2017-02-10 19:56:09 -0500

    Cleaned Up Winch Code
    Minor clean up

--- src/main/java/ca/_4976/steamworks/subsystems/Winch.java
+++ src/main/java/ca/_4976/steamworks/subsystems/Winch.java
@@ -1,48 +1,40 @@
 package ca._4976.steamworks.subsystems;
 
-import ca._4976.library.controllers.Axis;
-import ca._4976.library.listeners.BooleanListener;
+import ca._4976.library.AsynchronousRobot;
 import ca._4976.library.listeners.ButtonListener;
-import ca._4976.library.listeners.DoubleListener;
 import ca._4976.steamworks.Robot;
-import com.sun.org.apache.xml.internal.dtm.ref.DTMAxisIterNodeList;
-
-import javax.xml.bind.Marshaller;
+import edu.wpi.first.wpilibj.*;
 
 /**
- * Created by User on 31/01/2017.
+ * Created by Grant on 2/2/2017.
  */
-public class Winch {
+public class LazySusan extends AsynchronousRobot{
 
-    boolean winchMotor = false;
+    int vision_state = 0;
 
-    public Winch(Robot module) {
-        //WRITE CODE HERE
-
-        module.driver.Y.addListener(new ButtonListener() {
+    public LazySusan(Robot module){
+        module.driver.BACK.addListener(new ButtonListener() {
             @Override
-            public void pressed() {
-                winchMotor = !winchMotor;
-                module.outputs.winchLeft.set(winchMotor ? 1 : 0);
-                module.outputs.winchRight.set(winchMotor ? 1 : 0);
-                //turns winch on if true off if false
-                module.outputs.winchArm.output(true);
+            public void falling() {
+                if(vision_state == 0) {
+                    vision_state = 2;
+                    System.out.println("Seeing target");
+                }
+                else if(vision_state == 2) {
+                    vision_state = 0;
+                    System.out.println("Not seeing target");
+                }
             }
         });
-        module.operator.LV.addListener(new DoubleListener() {
-            @Override
-            public void changed(double value) {
-                module.outputs.winchLeft.set(value);
-                module.outputs.winchRight.set(value);
-            }
-        });
-        module.inputs.winchSensor.addListener(new BooleanListener() {
-            @Override
-            public void changed() {
-                module.outputs.winchLeft.set(0);
-                module.outputs.winchRight.set(0);
+
+        //call midura's code
+        module.driver.A.addListener(new ButtonListener() {
+            @Override public void rising() {
+                if (vision_state == 0) {
+                    module.runNextLoop(() -> module.driver.setRumble(1), 0);
+                    module.runNextLoop(() -> module.driver.setRumble(0), 3000);
+                }
             }
         });
     }
 }
-
