873e54d9338b325805016fc3c540efce70977b13
Jackrey <jack@rockyroost.com>
2017-02-14 19:42:20 -0500

    Update ElevatorBackUp.java

--- src/main/java/ca/_4976/steamworks/subsystems/ElevatorBackUp.java
+++ src/main/java/ca/_4976/steamworks/subsystems/ElevatorBackUp.java
@@ -1,6 +1,7 @@
 package ca._4976.steamworks.subsystems;
 
 import ca._4976.library.listeners.BooleanListener;
+import ca._4976.library.listeners.ButtonListener;
 import ca._4976.steamworks.Robot;
 import ca._4976.steamworks.io.Inputs;
 import ca._4976.steamworks.io.Outputs;
@@ -10,8 +11,7 @@ import ca._4976.steamworks.io.Outputs;
  */
 public class ElevatorBackUp {
 
-    public Boolean isShooting = false, cockingShooter = false;
-
+    public boolean elevatorUserControl = false;
     private Outputs outputs;
     private Inputs inputs;
 
@@ -20,7 +20,6 @@ public class ElevatorBackUp {
         outputs = module.outputs;
         inputs = module.inputs;
 
-        if(!isShooting && !cockingShooter) {
             module.inputs.bottomOfHE.addListener(new BooleanListener() {
                 @Override
                 public void rising() {
@@ -38,16 +37,39 @@ public class ElevatorBackUp {
                 }
             });
 
+        module.operator.LB.addListener(new ButtonListener() {
+            @Override
+            public void rising() {
+                runHE(1);
+                elevatorUserControl = true;
+            }
+        });
 
+        module.operator.LB.addListener(new ButtonListener() {
+            @Override
+            public void falling() {
+                stopMotors();
+                elevatorUserControl = false;
+            }
+        });
 
-            module.inputs.topOfSHE.addListener(new BooleanListener() {
-                @Override
-                public void rising() {
-                    stopMotors();
-                }
-            });
-        }
-    }
+        module.operator.RB.addListener(new ButtonListener() {
+            @Override
+            public void rising() {
+                runSHE(1);
+                elevatorUserControl = true;
+            }
+        });
+
+        module.operator.RB.addListener(new ButtonListener() {
+            @Override
+            public void falling() {
+                stopMotors();
+                elevatorUserControl = false;
+            }
+        });
+}
+
 
     public void runHE(double speed){outputs.hopperElevator.pidWrite(speed);}
 
@@ -55,12 +77,13 @@ public class ElevatorBackUp {
 
     public void stopMotors(){runHE(0); runSHE(0);}
 
-    public void fireElevator(){runHE(1); runSHE(1); isShooting = true;}
+    public void fire(){runHE(1); runSHE(1);}
 
     public void cockingSetup(){
         runHE(1);
-        if(inputs.topOfSHE.get()){
-
+        if(!inputs.topOfSHE.get()){
+            runHE(1);
+            runSHE(1);
         }
     }
 
