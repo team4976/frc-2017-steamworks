d690c7e9ccc60d749d1e7730a9bd00ef9a4a72d2
Marc Levesque <qormix@gmail.com>
2017-02-20 19:49:57 -0500

    Motor ramping update

--- src/main/java/ca/_4976/steamworks/subsystems/DriveTrain.java
+++ src/main/java/ca/_4976/steamworks/subsystems/DriveTrain.java
@@ -1,36 +1,87 @@
 package ca._4976.steamworks.subsystems;
 
+import ca._4976.library.Evaluable;
 import ca._4976.library.math.Vector2D;
 import ca._4976.steamworks.Robot;
 
 public class DriveTrain {
 
-    private Robot module;
-
-    private Vector2D velocity = new Vector2D(0, 0);
-
     public DriveTrain(Robot module) {
 
-        this.module = module;
+        Vector2D targetVelocity = new Vector2D(0, 0);
+        Vector2D setVelocity = new Vector2D(0, 0);
+
+        Vector2D linearRamp = new Vector2D(0.01, 0.02);
+        Vector2D rotationalRamp = new Vector2D(0.02, 0.03);
+
+        Evaluable updateDrive = new Evaluable() {
+
+            @Override public void eval() {
+
+                double diffX = targetVelocity.getX() - setVelocity.getX();
+                double diffY = targetVelocity.getY() - setVelocity.getY();
+
+                if ((setVelocity.getX() >= 0 && targetVelocity.getX() > setVelocity.getX()) ||
+                        (setVelocity.getX() <= 0 && targetVelocity.getX() < setVelocity.getX()))
+
+                    if (Math.abs(diffX) <= linearRamp.getX()) setVelocity.setX(targetVelocity.getX());
+
+                    else if (setVelocity.getX() < targetVelocity.getX()) setVelocity.setX(setVelocity.getX() + linearRamp.getX());
+
+                    else setVelocity.setX(setVelocity.getX() - linearRamp.getX());
+
+                else
+
+                if (Math.abs(diffX) <= linearRamp.getY()) setVelocity.setX(targetVelocity.getX());
+
+                else if (setVelocity.getX() < targetVelocity.getX()) setVelocity.setX(setVelocity.getX() + linearRamp.getY());
+
+                else setVelocity.setX(setVelocity.getX() - linearRamp.getY());
+
+                if ((setVelocity.getY() >= 0 && targetVelocity.getY() > setVelocity.getY()) ||
+                        (setVelocity.getY() <= 0 && targetVelocity.getY() < setVelocity.getY()))
+
+                    if (Math.abs(diffY) <= rotationalRamp.getX()) setVelocity.setY(targetVelocity.getY());
+
+                    else if (setVelocity.getY() < targetVelocity.getY()) setVelocity.setY(setVelocity.getY() + rotationalRamp.getX());
+
+                    else setVelocity.setY(setVelocity.getY() - rotationalRamp.getX());
+
+                else
+
+                if (Math.abs(diffY) <= rotationalRamp.getY()) setVelocity.setY(targetVelocity.getY());
+
+                else if (setVelocity.getY() < targetVelocity.getY()) setVelocity.setY(setVelocity.getY() + rotationalRamp.getY());
+
+                else setVelocity.setY(setVelocity.getY() - rotationalRamp.getY());
+
+                module.outputs.driveLeftFront.set(-setVelocity.getX() - setVelocity.getY());
+                module.outputs.driveLeftRear.set(-setVelocity.getX() - setVelocity.getY());
+                module.outputs.driveRightFront.set(-setVelocity.getX() + setVelocity.getY());
+                module.outputs.driveRightRear.set(-setVelocity.getX() + setVelocity.getY());
+
+                module.runNextLoop(this);
+            }
+        };
 
         module.driver.LH.addListener(value -> {
 
-            velocity.setX(value > 0 ? value * value : value * -value);
-            drive();
+            if (targetVelocity.getX() != targetVelocity.getX()) {
+
+                targetVelocity.setX(value > 0 ? value * value : value * -value);
+                updateDrive.eval();
+
+            } else targetVelocity.setX(value > 0 ? value * value : value * -value);
         });
 
         module.driver.BT.addListener(value -> {
 
-            velocity.setY(value);
-            drive();
+            if (targetVelocity.getY() != targetVelocity.getY()) {
+
+                targetVelocity.setY(value);
+                updateDrive.eval();
+
+            } else targetVelocity.setX(value);
         });
-    }
-
-    private void drive() {
-
-        module.outputs.driveLeftFront.set(-velocity.getX() - velocity.getY() - 0.01);
-        module.outputs.driveLeftRear.set(-velocity.getX() - velocity.getY() - 0.01);
-        module.outputs.driveRightFront.set(-velocity.getX() + velocity.getY() + 0.01);
-        module.outputs.driveRightRear.set(-velocity.getX() + velocity.getY() + 0.01);
     }
 }
