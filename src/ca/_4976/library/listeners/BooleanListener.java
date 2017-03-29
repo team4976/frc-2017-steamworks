a52ca3bfb5e8dd2957ee0f3dbc4e7a3cc2cbc7ac
Marc Levesque <Qormix@gmail.com>
2017-03-28 22:42:00 -0400

    Clean Up.

--- src/ca/_4976/library/listeners/BooleanListener.java
+++ src/ca/_4976/library/listeners/BooleanListener.java
@@ -2,7 +2,7 @@ package ca._4976.library.listeners;
 
 public abstract class BooleanListener {
 
-    public void falling() { }
+    public abstract void falling();
 
-    public void rising() { }
+    public abstract void rising();
 }
